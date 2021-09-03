71fdf50a18edfd5b34e85c5a297038572f2ffa2e
abhishek <abhishek.rauthan.10@gmail.com>
2021-09-03 13:46:44 +0530

    made some ui changes in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,14 +7,14 @@ const Navbar: FC = () => (
     <Flex
       position="sticky"
       zIndex="sticky"
-      backgroundColor="gray.900"
+      backgroundColor="gray.50"
       paddingY="4"
       paddingX="16"
       align="center"
       borderBottom="1px"
       borderColor="gray.200"
     >
-      <CustomIcon h="10" w="10" color="green.400" marginRight="5">
+      <CustomIcon h="10" w="10" color="green.500" marginRight="5">
         <path
           fillRule="evenodd"
           fill="currentColor"
@@ -22,7 +22,7 @@ const Navbar: FC = () => (
           clipRule="evenodd"
         />
       </CustomIcon>
-      <Heading ml="2" size="lg" color="gray.400">
+      <Heading ml="2" size="lg" color="gray.700">
         SupaDocs
       </Heading>
       <Flex
@@ -31,22 +31,32 @@ const Navbar: FC = () => (
         ml="5"
         px="3"
         py="1"
-        backgroundColor="gray.700"
-        color="gray.400"
+        backgroundColor="gray.200"
+        
         borderRadius="lg"
         border="2px"
         borderColor="transparent"
         _focusWithin={{
           borderColor: "green.400",
+          shadow:"md"
         }}
       >
         <Input
           placeholder="Search"
           flexGrow={1}
           backgroundColor="transparent"
+          color="gray.800"
           fontSize="lg"
           border="none"
           focusBorderColor="transparent"
+          _placeholder={{
+            color: "gray.400"
+          }}
+          _focus={{
+            _placeholder: {
+              color: "gray.800"
+            }
+          }}
         ></Input>
       </Flex>
     </Flex>
@@ -56,35 +66,7 @@ const Navbar: FC = () => (
 export default Navbar;
 
 {
-  /* <header className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
-<Button
-  color="gray"
-  buttonType="outline"
-  rounded={true}
-  iconOnly={true}
-  ripple="dark"
-  className="h-20 w-20 border-0"
->
-  <Icon name="menu" size="3xl" />
-</Button>
-<Icon
-  name="description"
-  color="blue"
-  size="5xl"
-  className="hidden md:inline-flex pr-5"
-/>
-<h1 className="ml-2 text-gray-700 text-2xl hidden md:inline-flex">
-  Docs
-</h1>
-<div className="flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:shadow-md">
-  <Icon name="search" size="3xl" color="gray" />
-  <input
-    type="text"
-    placeholder="Search"
-    className="flex-grow px-5 text-base bg-transparent outline-none"
-  />
-</div>
-
+  /* 
 <Button
   color="gray"
   buttonType="outline"
