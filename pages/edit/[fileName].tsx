aad315232df1d68f7a436fc86c049b46d18b8c0b
abhishek <abhishek.rauthan.10@gmail.com>
2021-09-07 16:43:44 +0530

    added Editor

--- pages/edit/[fileName].tsx
+++ pages/edit/[fileName].tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { ComponentType, useState } from "react";
+import dynamic from "next/dynamic";
 import Navbar from "@components/Navbar";
-import { Editor, EditorState } from "draft-js";
+import { EditorState } from "draft-js";
+import { EditorProps } from 'react-draft-wysiwyg'
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const Editor = dynamic<EditorProps>(
+  () => import("react-draft-wysiwyg").then((module) => module.Editor),
+  {
+    ssr: false,
+  }
+);
 
 const FileEditor = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (editorState: EditorState) => {
+    setEditorState(editorState);
+  };
   return (
     <>
       <Navbar />
-      <Box as="section" minH="7xl">
-        <Editor  editorState={editorState} onChange={setEditorState} />;
-      </Box>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={onEditorStateChange}
+        toolbarClassName=""
+      />
     </>
   );
 };
