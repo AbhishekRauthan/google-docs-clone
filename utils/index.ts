4676263999228c5ce233cd43bdc98f9a27a83398
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-10-19 14:03:11 +0530

    the following changes were done:
      - exported a function that validates given email and returns boolean true or false
      - exported a function that gets a toast function and description string and return an error toast

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,22 @@
+import { UseToastOptions } from '@chakra-ui/toast';
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export default createClient(supabaseUrl, supabaseAnonKey)
+
+export function validateEmail(email: string) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(email).toLowerCase());
+}
+
+export function useErrorToast(toast: (options?: UseToastOptions) => string | number, descrp: string) {
+  return toast({
+    status: "error",
+    title: "Error",
+    description: String(descrp),
+    isClosable: true,
+  });
+}
