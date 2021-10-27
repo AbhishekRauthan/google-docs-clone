4c07b384b60f5dac0ea258b70c8deedb5c34ccf2
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-10-27 21:09:34 +0000

    removed width as default from Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -10,7 +10,6 @@ const Input = forwardRef<HTMLInputElement, ChakraInputProps>(
       <ChakraInput
         focusBorderColor="green.400"
         variant="flushed"
-        w={"120%"}
         display="block"
         fontSize={{ base: "lg", md: "xl" }}
         ref={ref}
